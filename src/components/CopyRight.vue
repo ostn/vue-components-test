59fc639f3a421c9df6397ceedb6aa32d55324e92
ostn <9665466@qq.com>
2019-01-14 18:47:33 +0800

    Tips: 初版

---
+++ src/components/CopyRight.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<a href="https://www.vjoke.cn" target="_blank">关于微笑话</a>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
